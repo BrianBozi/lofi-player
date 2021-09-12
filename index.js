751cd5873567e583dcb3cd0cdff2b0e6f5aec46d
Brian Bozigian <bbozigian@outlook.com>
2021-09-12 19:46:43 +0000

    changed background to shooting stars, thanks @alphardex, added functionality to play and pause buttons

---
+++ index.js
@@ -0,0 +1,43 @@
+const playBtn = document.querySelector(".play")
+const nextBtn = document.querySelector(".next")
+const prevBtn = document.querySelector(".previous")
+const pauseBtn = document.querySelector(".pause")
+const albumArt = document.querySelector(".albumArt")
+const song = document.querySelector(".songs")
+
+const songList = [
+  {
+    song: "Lofi Mando",
+    img: "./music/img/mando-lofi.jpeg",
+    src: "./music/songs/lofi-mando.mp3"
+  },
+  {
+    song: "",
+    img: "./music/img/colors-minial.jpeg",
+    src: "./music/songs/colors-lofi.mp3"
+  }
+]
+
+indexNum = 1
+
+window.addEventListener('load', () => {
+  play(indexNum)
+})
+
+function play (songIndex){
+  albumArt.src = `${songList[indexNum - 1].img}`
+}
+
+
+playBtn.addEventListener('click', function (event) {
+  playBtn.className = "hidden"
+  pauseBtn.className = "pause"
+  song.src = `${songList[indexNum - 1].src}`
+  song.play()
+})
+
+pauseBtn.addEventListener('click', function (event) {
+  playBtn.className = "play"
+  pauseBtn.className = "hidden"
+  song.pause()
+})
